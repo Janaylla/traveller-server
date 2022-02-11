aed9fdb9798a44b4971a3c8aedc7391005fcc6ba
Janaylla <janayllaf@gmail.com>
2022-02-11 20:59:46 -0300

    Finalizando

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,15 +3,13 @@ import multer from 'multer';
 
 import uploadConfig from '../config/upload';
 import { changeImage } from '../endpoint/changeImage';
-import { deleteImage } from '../endpoint/deleteImage';
 import { getUser } from '../endpoint/getUser';
-import { uploadImage } from '../endpoint/uploadImage';
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.post('/', upload.single('image'), uploadImage);
-routes.delete('/:filename', deleteImage);
+// routes.post('/', upload.single('image'), uploadImage);
+// routes.delete('/:filename', deleteImage);
 
 
 routes.put('/user/image/:id', upload.single('image'), changeImage);
